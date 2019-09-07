e08126ba2304b1ea63fcb1602f1c36c766df68a6
鈴木大輝 <suzuhiro25@gmail.com>
2019-09-07 18:24:22 +0900

    [Finish]Bookers1

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,52 @@
+class BooksController < ApplicationController
+
+  def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+
+  def top
+  end
+
+  def show
+  	@book = Book.find(params[:id])
+  end
+
+  def create
+  		@book = Book.new(book_params)
+        if @book.save
+          flash[:notice] = "successfully"
+          redirect_to book_path(@book)
+        else
+          @books = Book.all
+      	  render action: :index
+      	end
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+    if @book.update(book_params)
+    	flash[:notice] = "successfully"
+    	redirect_to book_path(@book)
+    else
+    	flash[:notice] = "error"
+    	@book = Book.find(params[:id])
+    	render action: :edit
+    end
+  end
+
+  def  destroy
+  	 book = Book.find(params[:id])
+     book.destroy
+     redirect_to books_path
+  end
+
+    private
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
