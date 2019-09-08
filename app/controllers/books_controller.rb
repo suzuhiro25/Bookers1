0115a868ba0f512ab78e427a3d0a9247b0f2f60c
鈴木大輝 <suzuhiro25@gmail.com>
2019-09-08 16:26:49 +0900

    [Fix]root_pathの指定と不要な引数の削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,7 +15,7 @@ class BooksController < ApplicationController
   def create
   		@book = Book.new(book_params)
         if @book.save
-          flash[:notice] = "successfully"
+          flash[:notice] = "Book was successfully created"
           redirect_to book_path(@book)
         else
           @books = Book.all
@@ -30,7 +30,7 @@ class BooksController < ApplicationController
   def update
   	@book = Book.find(params[:id])
     if @book.update(book_params)
-    	flash[:notice] = "successfully"
+    	flash[:notice] = " Book was successfully updated"
     	redirect_to book_path(@book)
     else
     	flash[:notice] = "error"
@@ -41,8 +41,10 @@ class BooksController < ApplicationController
 
   def  destroy
   	 book = Book.find(params[:id])
-     book.destroy
+     if book.destroy
+      flash[:notice] = "Book was successfully destroyed"
      redirect_to books_path
+   end
   end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
   get "/books" => "books#index"
-  get "/" => "books#top"
+  root  "books#top"
 end
